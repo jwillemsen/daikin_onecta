fdf7093e2272f2b8a74ec08ee6c4208c80193527
Johnny Willemsen <jwillemsen@remedy.nl>
2023-02-20 12:11:32 +0100

    Remove empty line
        * custom_components/daikin_residential_altherma/water_heater.py:

--- custom_components/daikin_residential_altherma/water_heater.py
+++ custom_components/daikin_residential_altherma/water_heater.py
@@ -36,7 +36,6 @@ HA_TANK_ATTR_TO_DAIKIN = {
     ATTR_TANK_TARGET_TEMPERATURE: ATTR_TANK_TARGET_TEMPERATURE,
 }
 
-
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Old way of setting up the Daikin HVAC platform.
 
