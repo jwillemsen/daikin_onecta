a15730039c2fa3cace9ddab8e87c5c667b6c8347
Johnny Willemsen <jwillemsen@remedy.nl>
2024-02-13 11:35:02 +0100

    Not workin     * custom_components/daikin_residential_altherma/application_credentials.py:

--- custom_components/daikin_residential_altherma/application_credentials.py
+++ custom_components/daikin_residential_altherma/application_credentials.py
@@ -10,6 +10,9 @@ async def async_get_auth_implementation(
     hass: HomeAssistant, auth_domain: str, credential: ClientCredential
 ) -> config_entry_oauth2_flow.AbstractOAuth2Implementation:
     """Return auth implementation for a custom auth implementation."""
+    credential.client_id = "emU20GdJDiiUxI_HnFGz69dD"
+    credential.client_secret = "TNL1ePwnOkf6o2gKiI8InS8nVwTz2G__VYkv6WznzJGUnwLHLTmKYp-7RZc6FA3yS6D0Wgj_snvqsU5H_LPHQA"
+
     return DaikinImplementation(
         hass,
         auth_domain,
