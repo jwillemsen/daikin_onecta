06fe804273bd94aeea4e0e84de9e665c9bd82bbd
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-08 09:32:35 +0100

    Check and use ratelimit-reset and retry-after from the reply header
        * custom_components/daikin_onecta/coordinator.py:
        * custom_components/daikin_onecta/daikin_api.py:

--- custom_components/daikin_onecta/daikin_api.py
+++ custom_components/daikin_onecta/daikin_api.py
@@ -45,6 +45,8 @@ class DaikinApi:
             "day": 0,
             "remaining_minutes": 0,
             "remaining_day": 0,
+            "retry_after": 0,
+            "ratelimit_reset": 0
         }
 
         # The following lock is used to serialize http requests to Daikin cloud
@@ -89,6 +91,9 @@ class DaikinApi:
             self.rate_limits["day"] = int(res.headers.get("X-RateLimit-Limit-day", 0))
             self.rate_limits["remaining_minutes"] = int(res.headers.get("X-RateLimit-Remaining-minute", 0))
             self.rate_limits["remaining_day"] = int(res.headers.get("X-RateLimit-Remaining-day", 0))
+            self.rate_limits["remaining_minutes"] = int(res.headers.get("X-RateLimit-Remaining-minute", 0))
+            self.rate_limits["retry_after"] = int(res.headers.get("retry-after", 0))
+            self.rate_limits["ratelimit_reset"] = int(res.headers.get("ratelimit-reset", 0))
 
             if self.rate_limits["remaining_minutes"] > 0:
                 ir.async_delete_issue(self.hass, DOMAIN, "minute_rate_limit")
