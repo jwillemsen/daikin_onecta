90b5fbe1a21fe320eba5c4e0f7da608cbadc3545
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-12 14:29:17 +0100

    Enable all code
        * tests/test_config_flow.py:

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -27,12 +27,12 @@ CLIENT_SECRET = "5678"
 async def setup_credentials(hass: HomeAssistant) -> None:
     """Fixture to setup credentials."""
     assert await async_setup_component(hass, "application_credentials", {})
-    # await async_import_client_credential(
-    #     hass,
-    #     DOMAIN,
-    #     ClientCredential(CLIENT_ID, CLIENT_SECRET),
-    #     DOMAIN,
-    # )
+    await async_import_client_credential(
+        hass,
+        DOMAIN,
+        ClientCredential(CLIENT_ID, CLIENT_SECRET),
+        DOMAIN,
+    )
 
 
 async def test_full_flow(
@@ -44,39 +44,39 @@ async def test_full_flow(
 ) -> None:
     """Check full flow."""
     assert await async_setup_component(hass, "daikin_onecta", {})
-    #
-    # await async_import_client_credential(hass, DOMAIN, ClientCredential(CLIENT_ID, CLIENT_SECRET))
-    #
-    # result = await hass.config_entries.flow.async_init("daikin_onecta", context={"source": config_entries.SOURCE_USER})
-    # state = config_entry_oauth2_flow._encode_jwt(
-    #     hass,
-    #     {
-    #         "flow_id": result["flow_id"],
-    #         "redirect_uri": "https://example.com/auth/external/callback",
-    #     },
-    # )
-    #
-    # assert result["url"] == (
-    #     f"{OAUTH2_AUTHORIZE}?response_type=code&client_id={CLIENT_ID}" "&redirect_uri=https://example.com/auth/external/callback" f"&state={state}"
-    # )
-    #
-    # client = await hass_client_no_auth()
-    # resp = await client.get(f"/auth/external/callback?code=abcd&state={state}")
-    # assert resp.status == 200
-    # assert resp.headers["content-type"] == "text/html; charset=utf-8"
-    #
-    # aioclient_mock.post(
-    #     OAUTH2_TOKEN,
-    #     json={
-    #         "refresh_token": "mock-refresh-token",
-    #         "access_token": "mock-access-token",
-    #         "type": "Bearer",
-    #         "expires_in": 60,
-    #     },
-    # )
-    #
-    # with patch("homeassistant.components.daikin_onecta.async_setup_entry", return_value=True) as mock_setup:
-    #     await hass.config_entries.flow.async_configure(result["flow_id"])
-    #
-    # assert len(hass.config_entries.async_entries(DOMAIN)) == 1
-    # assert len(mock_setup.mock_calls) == 1
+
+    await async_import_client_credential(hass, DOMAIN, ClientCredential(CLIENT_ID, CLIENT_SECRET))
+
+    result = await hass.config_entries.flow.async_init("daikin_onecta", context={"source": config_entries.SOURCE_USER})
+    state = config_entry_oauth2_flow._encode_jwt(
+        hass,
+        {
+            "flow_id": result["flow_id"],
+            "redirect_uri": "https://example.com/auth/external/callback",
+        },
+    )
+
+    assert result["url"] == (
+        f"{OAUTH2_AUTHORIZE}?response_type=code&client_id={CLIENT_ID}" "&redirect_uri=https://example.com/auth/external/callback" f"&state={state}"
+    )
+
+    client = await hass_client_no_auth()
+    resp = await client.get(f"/auth/external/callback?code=abcd&state={state}")
+    assert resp.status == 200
+    assert resp.headers["content-type"] == "text/html; charset=utf-8"
+
+    aioclient_mock.post(
+        OAUTH2_TOKEN,
+        json={
+            "refresh_token": "mock-refresh-token",
+            "access_token": "mock-access-token",
+            "type": "Bearer",
+            "expires_in": 60,
+        },
+    )
+
+    with patch("homeassistant.components.daikin_onecta.async_setup_entry", return_value=True) as mock_setup:
+        await hass.config_entries.flow.async_configure(result["flow_id"])
+
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    assert len(mock_setup.mock_calls) == 1
