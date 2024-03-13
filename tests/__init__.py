730906ab4797b7b736d3e5d8e1caa85e5db1dd70
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-13 12:35:22 +0100

    Add todo
        * tests/__init__.py:

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,6 @@
 """Tests for the daikin_onecta integration."""
+
+# TODO:
+# - Test rate limits
+# - Test various sensors, provide test json for various devices
+# - Test commands to devices with cache updates
