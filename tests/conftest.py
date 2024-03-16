a554d241ee8bf2f84ff61ad6534fe437dad05213
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-16 16:16:21 +0100

    Add file

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+"""Global fixtures for myenergi integration."""
+from typing import Any
+from unittest.mock import patch
+
+import pytest
+
+pytest_plugins = "pytest_homeassistant_custom_component"
+
+
+@pytest.fixture(name="auto_enable_custom_integrations", autouse=True)
+def auto_enable_custom_integrations(
+    hass: Any, enable_custom_integrations: Any  # noqa: F811
+) -> None:
+    """Enable custom integrations defined in the test dir."""
+
+
+# This fixture is used to prevent HomeAssistant from attempting to create and dismiss persistent
+# notifications. These calls would fail without this fixture since the persistent_notification
+# integration is never loaded during a test.
+@pytest.fixture(name="skip_notifications", autouse=True)
+def skip_notifications_fixture():
+    """Skip notification calls."""
+    with patch("homeassistant.components.persistent_notification.async_create"), patch(
+        "homeassistant.components.persistent_notification.async_dismiss"
+    ):
+        yield
