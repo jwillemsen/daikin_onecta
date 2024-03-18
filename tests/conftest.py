e2849710ee4189820c5660e0b6b774916a4bb117
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-18 13:42:33 +0100

    Precommit
        * tests/conftest.py:

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,7 @@ from syrupy import SnapshotAssertion
 
 from . import load_fixture_json
 from _pytest.assertion import truncate
+
 truncate.DEFAULT_MAX_LINES = 9999
 truncate.DEFAULT_MAX_CHARS = 9999
 
