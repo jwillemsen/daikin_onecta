e6e568c6ba98da3be68ba4f8ef6d698406803b6f
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-19 09:15:13 +0100

    First services tests, need to stub the low level API calls
        * tests/snapshots/test_init.ambr:
        * tests/test_init.py:

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,11 +2,18 @@
 from unittest.mock import AsyncMock
 
 import homeassistant.helpers.entity_registry as er
+from homeassistant.components.water_heater import ATTR_OPERATION_MODE
+from homeassistant.components.water_heater import ATTR_TEMPERATURE
+from homeassistant.components.water_heater import DOMAIN as WATER_HEATER_DOMAIN
+from homeassistant.components.water_heater import SERVICE_SET_OPERATION_MODE
+from homeassistant.components.water_heater import SERVICE_SET_TEMPERATURE
+from homeassistant.components.water_heater import STATE_PERFORMANCE
+from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.const import Platform
+from homeassistant.const import STATE_OFF
 from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 from syrupy import SnapshotAssertion
-from homeassistant.components.water_heater import STATE_PERFORMANCE
 
 from .conftest import snapshot_platform_entities
 
@@ -21,6 +28,7 @@ async def test_altherma(
     """Test entities."""
     await snapshot_platform_entities(hass, config_entry, Platform.SENSOR, entity_registry, snapshot, "altherma")
 
+
 async def test_mc80z(
     hass: HomeAssistant,
     config_entry: MockConfigEntry,
@@ -30,6 +38,7 @@ async def test_mc80z(
 ) -> None:
     """Test entities."""
     await snapshot_platform_entities(hass, config_entry, Platform.SENSOR, entity_registry, snapshot, "mc80z")
+
 
 async def test_altherma_boost(
     hass: HomeAssistant,
@@ -43,3 +52,19 @@ async def test_altherma_boost(
     await snapshot_platform_entities(hass, config_entry, Platform.SENSOR, entity_registry, snapshot, "altherma_boost")
 
     assert hass.states.get("water_heater.altherma").attributes["operation_mode"] == STATE_PERFORMANCE
+
+    await hass.services.async_call(
+        WATER_HEATER_DOMAIN,
+        SERVICE_SET_TEMPERATURE,
+        {ATTR_ENTITY_ID: "water_heater.altherma", ATTR_TEMPERATURE: 58},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    await hass.services.async_call(
+        WATER_HEATER_DOMAIN,
+        SERVICE_SET_OPERATION_MODE,
+        {ATTR_ENTITY_ID: "water_heater.altherma", ATTR_OPERATION_MODE: STATE_OFF},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
