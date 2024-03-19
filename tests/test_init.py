5e44e569cfdcf5d31c9732db6ed290d45880eed0
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-19 08:47:00 +0100

    Add test case with altherma with boost enabled, check that we get a performance state
        * tests/fixtures/altherma_boost.json:
          Added.
    
        * tests/snapshots/test_init.ambr:
        * tests/test_init.py:

--- tests/test_init.py
+++ tests/test_init.py
@@ -6,6 +6,7 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 from syrupy import SnapshotAssertion
+from homeassistant.components.water_heater import STATE_PERFORMANCE
 
 from .conftest import snapshot_platform_entities
 
@@ -29,3 +30,16 @@ async def test_mc80z(
 ) -> None:
     """Test entities."""
     await snapshot_platform_entities(hass, config_entry, Platform.SENSOR, entity_registry, snapshot, "mc80z")
+
+async def test_altherma_boost(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    onecta_auth: AsyncMock,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test entities."""
+    # Altherma with boost enabled
+    await snapshot_platform_entities(hass, config_entry, Platform.SENSOR, entity_registry, snapshot, "altherma_boost")
+
+    assert hass.states.get("water_heater.altherma").attributes["operation_mode"] == STATE_PERFORMANCE
