9ce5f8af58070a316e3c3bb43e95b53ca94cb118
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-20 09:21:16 +0100

    More cleanup
        * tests/__init__.py:

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,24 +2,9 @@
 from __future__ import annotations
 
 import json
-import logging
-from typing import Any
-from unittest.mock import Mock
-from unittest.mock import patch
-
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from pytest_homeassistant_custom_component.common import MockConfigEntry
-
-from custom_components.daikin_onecta import DOMAIN
-
-
-TEST_CONFIG_ENTRY_ID = "77889900af"
 
 
 def load_fixture_json(name):
     with open(f"tests/fixtures/{name}.json") as json_file:
         data = json.load(json_file)
         return data
-
-
