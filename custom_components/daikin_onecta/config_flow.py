f2c26849a65b7379c733015d3944ad1cab85a7e7
Johnny Willemsen <jwillemsen@remedy.nl>
2024-05-07 09:10:58 +0200

    precommit
        * custom_components/daikin_onecta/config_flow.py:

--- custom_components/daikin_onecta/config_flow.py
+++ custom_components/daikin_onecta/config_flow.py
@@ -87,9 +87,7 @@ class FlowHandler(
     @property
     def extra_authorize_data(self) -> dict[str, str]:
         """Extra data that needs to be appended to the authorize url."""
-        return {
-            "scope": "openid onecta:basic.integration"
-        }
+        return {"scope": "openid onecta:basic.integration"}
 
     async def async_oauth_create_entry(self, data: dict) -> FlowResult:
         """Create an oauth config entry or update existing entry for reauth."""
