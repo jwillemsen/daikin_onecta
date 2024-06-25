e9df0e406c621062e7e0a6af72b413713a583a29
Johnny Willemsen <jwillemsen@remedy.nl>
2024-06-25 13:51:31 +0200

    precommit fixes
        * tests/test_init.py:

--- tests/test_init.py
+++ tests/test_init.py
@@ -88,6 +88,7 @@ async def test_dry2(
 
     assert hass.states.get("climate.bedroom_3_room_temperature").state == HVACMode.OFF
 
+
 async def test_schedule(
     hass: HomeAssistant,
     config_entry: MockConfigEntry,
@@ -99,6 +100,7 @@ async def test_schedule(
     await snapshot_platform_entities(hass, config_entry, Platform.SENSOR, entity_registry, snapshot, "schedule")
 
     assert hass.states.get("select.bureau_climatecontrol_schedule").state == "off"
+
 
 async def test_ururu(
     hass: HomeAssistant,
