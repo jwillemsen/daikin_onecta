b571c509801346e266e2054cc21978fa90383dca
Johnny Willemsen <jwillemsen@remedy.nl>
2024-10-31 20:29:55 +0100

    Remove not used constants
        * custom_components/daikin_onecta/const.py:

--- custom_components/daikin_onecta/const.py
+++ custom_components/daikin_onecta/const.py
@@ -25,12 +25,6 @@ DAIKIN_API = "daikin_api"
 DAIKIN_DEVICES = "daikin_devices"
 DAIKIN_API_URL = "https://api.onecta.daikineurope.com"
 
-ATTR_PRESET_MODE = "preset_mode"
-ATTR_OPERATION_MODE = "operation_mode"
-
-ATTR_STATE_ON = "on"
-ATTR_STATE_OFF = "off"
-
 SCHEDULE_OFF = "off"
 
 FANMODE_FIXED = "fixed"
@@ -40,8 +34,6 @@ SWING_FLOOR_HORIZONTAL = "floor_horizontal"
 SWING_COMFORT = "comfort"
 SWING_COMFORT_HORIZONTAL = "comfort_horizontal"
 
-SENSOR_TYPE_POWER = "power"
-SENSOR_TYPE_ENERGY = "energy"
 SENSOR_PERIOD_DAILY = "d"
 SENSOR_PERIOD_WEEKLY = "w"
 SENSOR_PERIOD_YEARLY = "m"
