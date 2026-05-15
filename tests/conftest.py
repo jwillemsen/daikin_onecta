7fc12441627139f3e70828c5790b105a758e64d1
Johnny Willemsen <jwillemsen@remedy.nl>
2026-05-15 17:23:30 +0200

    Freeze time for tests
        * tests/conftest.py:
        * tests/snapshots/test_init.ambr:

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ from unittest.mock import patch
 
 import homeassistant.helpers.entity_registry as er
 import pytest
+from freezegun import freeze_time
 from _pytest.assertion import truncate
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -41,7 +42,7 @@ def load_fixture_json(name):
 def auto_enable_custom_integrations(hass: Any, enable_custom_integrations: Any) -> None:  # noqa: F811
     """Enable custom integrations defined in the test dir."""
 
-
+@pytest.mark.freeze_time("2026-01-01 12:00:00+00:00")
 async def snapshot_platform_entities(
     hass: HomeAssistant,
     aioclient_mock: AiohttpClientMocker,
