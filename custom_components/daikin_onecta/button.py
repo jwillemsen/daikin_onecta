335b0a6e5c3109f3bcf8df0a941451f8fcddd5df
Johnny Willemsen <jwillemsen@remedy.nl>
2026-09-10 10:04:12 +0200

    Use immediate coordinator refresh for refresh button

--- custom_components/daikin_onecta/button.py
+++ custom_components/daikin_onecta/button.py
@@ -57,4 +57,4 @@ class DaikinRefreshButton(CoordinatorEntity, ButtonEntity):
         self.async_write_ha_state()
 
     async def async_press(self) -> None:
-        await self.coordinator.async_request_refresh()
+        await self.coordinator.async_refresh()
